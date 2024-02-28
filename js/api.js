e80f0ed9642431c94325572c06d7d76006338814
LillKonst <konstlillkristine@gmail.com>
2024-02-28 16:26:44 +0100

    API call and display of posts on explore page

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 export const NOROFF_API_URL = "https://v2.api.noroff.dev";
-
+/*
 export async function login(email, password) {
     if (!email.endsWith("@noroff.no") && !email.endsWith("@stud.noroff.no")) {
       return false;
@@ -44,3 +44,94 @@ export async function login(email, password) {
     localStorage.setItem("logged-in-email", email);
     return true;
   }
+
+*/
+
+
+//import {apiKey} from "../js/auth";
+
+//import { accessToken } from "../js/auth";
+
+//import { storeToken } from "../js/auth";
+
+import { getToken } from "../js/auth.js";
+
+//import { clearToken } from "../js/auth";
+
+import { apiKey } from "../js/auth.js";
+//export {headers};
+
+export {getAllPosts};
+
+// export {accessToken};
+/*
+function headers (includeContentType = true) {
+    const headers = {
+      Authorization: `Bearer ${getToken()}`,
+      "X-Noroff-API-Key": apiKey,
+    };
+  
+    if (includeContentType) {
+      headers["Content-Type"] = "application/json";
+    }
+  
+    return headers;
+  }*/
+
+//const apiKey = localStorage.setItem("apiKey");
+//console.log(apiKey);
+
+
+//Get all posts
+async function getAllPosts() {
+    const response = await fetch(
+        `${NOROFF_API_URL}/social/posts/?_author=true_comments=true&_reactions=true`,
+    {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getToken()}`,
+            "X-Noroff-API-Key": apiKey,
+        },
+        
+    }
+);
+    if (!response.ok) {
+        throw new Error("Could not load posts from following");
+    }
+    const result = await response.json();
+    return result.data;
+}
+
+//Get posts from following 
+async function getPostsFromFollowing() {
+    const response = await fetch(
+        `${NOROFF_API_URL}/social/posts/following?_author=true&_reactions=true&_comments=true`,
+    {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+            "X-Noroff-API-Key": apiKey
+        },
+    }
+);
+    if (!response.ok) {
+        throw new Error("Could not load posts from following");
+    }
+    const result = await response.json();
+    return result.data;
+}
+
+// Get post specific 
+async function getPostSpecific(postid) {
+    const response = await fetch(
+        `${NOROFF_API_URL}/social/posts/${postId}?_author=true&_reactions=true&_comments=true`,
+        {
+            headers: headers(),
+        }
+    );
+    if (!response.ok) {
+        throw new Error("Could not load post");
+    }
+    const result = await response.json();
+    return result.data;
+}
